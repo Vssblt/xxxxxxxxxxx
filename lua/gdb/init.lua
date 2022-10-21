76779bc271a0aefc33e82670ec9910e7acedce49
Vssblt <sunruiyangcp01@163.com>
2022-10-21 11:32:27 +0800

    new commi

--- lua/gdb/init.lua
+++ lua/gdb/init.lua
@@ -1,4 +1,9 @@
+local w = require('gdb.window');
+--local inspect = require('inspect')
 local i = {}
+local buf = {}
+local win = {}
+local job = {}
 GdbConfig = {
     project = {
       gdb = "/usr/bin/gdb",
@@ -12,25 +17,275 @@ GdbConfig = {
             "console",
             "gdb"
         },
-        size = 25
+        size = 18
       },
       {
         position = "right",
         window = {
             "scope",
             "breakpoints",
-            "stack"
+            "stack",
         },
-        size = 40
-      }
+        size = 50
+      },
     },
     log = "true",
     log_path = "/tmp/light-gdb.log",
   }
 
+
+  --{ {
+    --layout = "console",
+    --position = "bottom",
+    --size = 18,
+    --window = { "gdb" }
+  --}, {
+    --layout = "scope",
+    --position = "right",
+    --size = 50,
+    --window = { "breakpoints", "stack" }
+  --} }
+
+
+function i.open_console(position, pos_type, size)
+  local winid = nil
+  buf.console, winid = w.open({
+    pos = position,
+    size = size,
+    bufnr = buf.console,
+    pos_type = pos_type
+  })
+  return buf.console, winid
+end
+
+function i.open_scope(position, pos_type, size)
+  local winid = nil
+  buf.scope, winid = w.open({
+    pos = position,
+    size = size,
+    bufnr = buf.scope,
+    pos_type = pos_type
+  })
+  return buf.scope, winid
+end
+
+function i.open_gdb(position, pos_type, size)
+  local winid = nil
+  buf.gdb, winid = w.open({
+    pos = position,
+    size = size,
+    bufnr = buf.gdb,
+    pos_type = pos_type
+  })
+  return buf.gdb, winid
+end
+
+function i.open_breakpoints(position, pos_type, size)
+  local winid = nil
+  buf.breakpoints, winid = w.open({
+    pos = position,
+    size = size,
+    bufnr = buf.breakpoints,
+    pos_type = pos_type
+  })
+  return buf.breakpoints, winid
+end
+
+function i.open_stack(position, pos_type, size)
+  local winid = nil
+  buf.stack,winid = w.open({
+    pos = position,
+    size = size,
+    bufnr = buf.stack,
+    pos_type = pos_type
+  })
+  return buf.stack, winid
+end
+
 function i.setup(obj)
-  GdbConfig = GdbConfig or obj
+  obj = obj or GdbConfig
   require'gdb.log'.init()
+
+  -- restructuring
+  local layout_tree = {}
+  for index, item in pairs(obj.layout) do
+    table.insert(layout_tree, {})
+    layout_tree[index].layout = item.window[1]
+    layout_tree[index].window = {}
+    layout_tree[index].size = item.size
+    layout_tree[index].position = item.position
+    for index_2, name in pairs(item.window) do
+      if index_2 == 1 then
+        goto C
+      end
+      table.insert(layout_tree[index].window, name)
+      ::C::
+    end
+  end
+
+  local win_list = {}
+  for _, item in pairs(layout_tree) do
+    if item.layout == "console" then
+      table.insert(win_list, select(2, i.open_console(item.position, "absolute", item.size)))
+    elseif item.layout == "gdb" then
+      table.insert(win_list, select(2, i.open_gdb(item.position, "absolute", item.size)))
+    elseif item.layout == "scope" then
+      table.insert(win_list, select(2, i.open_scope(item.position, "absolute", item.size)))
+    elseif item.layout == "breakpoints" then
+      table.insert(win_list, select(2, i.open_breakpoints(item.position, "absolute", item.size)))
+    elseif item.layout == "stack" then
+      table.insert(win_list, select(2, i.open_stack(item.position, "absolute", item.size)))
+    end
+  end
+
+  for k1, item in pairs(layout_tree) do
+    local win_id = win_list[k1]
+    vim.api.nvim_set_current_win(win_id)
+    local position = (item.position == "bottom" or item.position == "top" ) and "right" or "top"
+    for _, v in pairs(item.window) do
+      w.open({pos = position, size = 0, pos_type = "relative"})
+    end
+  end
+    --vim.api.nvim_set_current_buf(window.gdb_bufnr)
+
+
+  --for _, item in pairs(obj.layout) do
+    --local pos_type = "absolute"
+    --local position = item.position
+    --local size = item.size
+    --for key, win_type in pairs(item.window) do
+      --if key == 1 then
+        --goto continue
+      --end
+      --if item.window[0] == "console" then
+
+      --end
+      --if win_type == "console" then
+        --buf.console, win.console = w.open({
+          --pos = position,
+          --size = size,
+          --bufnr = buf.console,
+          --pos_type = pos_type
+        --})
+      --elseif win_type == "gdb" then
+        --buf.gdb, win.gdb = w.open({
+          --pos = position,
+          --size = size,
+          --bufnr = buf.gdb,
+          --pos_type = pos_type
+        --})
+      --elseif win_type == "scope" then
+        --buf.scope, win.scope = w.open({
+          --pos = position,
+          --size = size,
+          --bufnr = buf.scope,
+          --pos_type = pos_type
+        --})
+      --elseif win_type == "breakpoints" then
+        --buf.breakpoints, win.breakpoints = w.open({
+          --pos = position,
+          --size = size,
+          --bufnr = buf.breakpoints,
+          --pos_type = pos_type
+        --})
+      --elseif win_type == "stack" then
+        --buf.stack, win.stack = w.open({
+          --pos = position,
+          --size = size,
+          --bufnr = buf.stack,
+          --pos_type = pos_type
+        --})
+      --end
+      --position = (item.position == "bottom" or item.position == "top" ) and "right" or "top"
+      --pos_type = "relative"
+      --size = 0
+      --::continue::
+    --end
+  --end
+
+  -- require('gdb.window').open_terminal(nil, { gdb_args = "-quiet -iex 'set pagination off' -iex 'set mi-async on' -ex 'echo startupdone\n'",app_path = vim.g.cpp_executable_program, pos = "bottom", size = 24})
+  --
+  -- {
+  --   gdb_args = "-quiet -iex 'set pagination off' -iex 'set mi-async on' -ex 'echo startupdone\n'",
+  --   app_path = vim.g.cpp_executable_program,
+  --   pos = "bottom", size = 24
+  -- }
+end
+
+function i.open_terminal(opts)
+  opts = opts or {
+      gdb = "/usr/bin/gdb",
+      gdb_args = '-quiet -iex "set pagination off" -iex "set mi-async on" -ex "echo startupdone\n"',
+      app_path = "",
+  }
+
+  local gdb = opts.gdb or "/usr/bin/gdb"
+  local gdb_args = opts.gdb_args or ""
+  local app_path = opts.app_path or ""
+
+  buf.gdb = buf.gdb or vim.api.nvim_create_buf(false, true)
+  buf.stdout = buf.stdout or vim.api.nvim_create_buf(false, true)
+  buf.mi = buf.mi or vim.api.nvim_create_buf(false, true)
+
+  local stdout_chan_id = vim.fn.jobstart({"cat", "-"}, {
+    pty = true,
+    on_stdout = function (_, msg, _)
+      io.write(buf.stdout .. '\n')
+      io.write(vim.api.nvim_buf_line_count(buf.stdout) .. '\n')
+      for _, line in pairs(msg) do
+        vim.fn.appendbufline(buf.stdout, vim.api.nvim_buf_line_count(buf.stdout), line)
+      end
+    end,
+    on_stderr = function (_, _, _)
+    end,
+    on_exit = function (_, _, _)
+      job.stdout_pty = nil
+      vim.cmd('bwipeout! ' .. buf.stdout)
+    end
+  })
+  job.stdout_pty = vim.api.nvim_get_chan_info(stdout_chan_id).pty
+  local mi_chan_id = vim.fn.jobstart({"cat", "-"}, {
+    pty = true,
+    on_stdout = function (_, msg, _)
+      io.write("mi_pty\n")
+      for _, line in pairs(msg) do
+        vim.fn.appendbufline(buf.stdout, vim.api.nvim_buf_line_count(buf.stdout), line)
+      end
+    end,
+    on_stderr = function (_, _, _)
+    end,
+    on_exit = function (_, _, _)
+      job.mi_pty = nil
+      vim.cmd('bwipeout! ' .. buf.mi)
+    end
+  })
+  job.mi_pty = vim.api.nvim_get_chan_info(mi_chan_id).pty
+
+  local cmd = gdb .. " " .. gdb_args .. " -tty " .. job.stdout_pty .. " " .. app_path
+
+  job.term_chan_id = vim.api.nvim_open_term(buf.gdb, {
+    on_input = function (_, _, _, data)
+      vim.api.nvim_chan_send( job.term_job_id, data)
+    end,
+  })
+
+  job.term_job_id = job.term_job_id or vim.fn.jobstart(cmd, {
+    height = vim.api.nvim_win_get_height(vim.api.nvim_get_current_win()),
+    width = vim.api.nvim_win_get_width(vim.api.nvim_get_current_win()),
+    pty = true,
+    on_exit = function (_, _, _)
+      job.term_job_id = nil
+      vim.cmd('bwipeout! ' .. buf.gdb)
+      vim.cmd('bwipeout! ' .. buf.stdout)
+      vim.cmd('bwipeout! ' .. buf.mi)
+    end,
+    on_stdout = function (_, msg)
+      vim.api.nvim_chan_send(job.term_chan_id, table.concat(msg, "\n"))
+    end
+  })
+  vim.api.nvim_chan_send(job.term_job_id, "server new-ui mi " .. job.mi_pty .. " \n")
+  vim.api.nvim_buf_set_option(buf.gdb, "filetype", "gdb-terminal")
+  return buf.gdb
 end
 
 return i
--- lua/gdb/window.lua
+++ lua/gdb/window.lua
@@ -1,30 +1,36 @@
 local window = {}
 
-function window:horizontal_layout(height, pos, pos_type, win_configs)
+function window.horizontal_layout(height, pos, pos_type, win_configs)
   local win_cmd
-  if pos_type == "relative" then
-    win_cmd = (pos == "top" and "lefta" or "bel") .. " split"
-  else
-    win_cmd = (pos == "top" and "topleft" or "botright") .. " split"
+  local size = ""
+  if height ~= 0 then
+    size = tostring(height)
   end
-  local win_id = window.create_window(window, win_cmd, win_configs)
-  vim.api.nvim_win_set_height(win_id, height)
+  if pos_type == "relative" then
+    win_cmd = (pos == "top" and "lefta" or "bel") .. " " .. size .. "split"
+  else
+    win_cmd = (pos == "top" and "topleft" or "botright") .. " " .. size .. "split"
+  end
+  local win_id = window.create_window(win_cmd, win_configs)
   return win_id
 end
 
-function window:vertical_layout(width, pos, pos_type, win_configs)
+function window.vertical_layout(width, pos, pos_type, win_configs)
   local win_cmd
-  if pos_type == "relative" then
-    win_cmd = (pos == "left" and "lefta" or "bel") .. " vsplit"
-  else
-    win_cmd = (pos == "left" and "topleft" or "botright") .. " vsplit"
+  local size = ""
+  if width ~= 0 then
+    size = tostring(width)
   end
-  local win_id = window.create_window(window, win_cmd, win_configs)
-  vim.api.nvim_win_set_width(win_id, width)
+  if pos_type == "relative" then
+    win_cmd = (pos == "left" and "lefta" or "bel") .. " " .. size .. "vsplit"
+  else
+    win_cmd = (pos == "left" and "topleft" or "botright") .. " " .. size .. "vsplit"
+  end
+  local win_id = window.create_window(win_cmd, win_configs)
   return win_id
 end
 
-function window:create_window(win_cmd, win_conf)
+function window.create_window(win_cmd, win_conf)
   win_conf = win_conf or {}
   vim.cmd(win_cmd)
   local win_id = vim.api.nvim_get_current_win()
@@ -34,117 +40,28 @@ function window:create_window(win_cmd, win_conf)
   return win_id
 end
 
-function window:open_terminal(opts)
-  opts = opts or {
-      gdb = "/usr/bin/gdb",
-      gdb_args = '-quiet -iex "set pagination off" -iex "set mi-async on" -ex "echo startupdone\n"',
-      app_path = "",
-      pos = "bottom",
-      pos_type = "relative"
-  }
-
-  local gdb = opts.gdb or "/usr/bin/gdb"
-  local gdb_args = opts.gdb_args or ""
-  local app_path = opts.app_path or ""
-  local pos = opts.pos or "bottom"
-  local pos_type = opts.pos_type or "absolute"
-
-  local size = opts.size or 20
-  local gdb_win_id
-  if pos == "bottom" or pos == "top" then
-    gdb_win_id = window.horizontal_layout(window, size, pos, pos_type, {
-    list = false,
-    relativenumber = false,
-    number = false,
-    winfixwidth = true,
-    winfixheight = true,
-    wrap = false,
-    signcolumn = "auto"
-  })
-  else
-    gdb_win_id = window.vertical_layout(window, size, pos, pos_type, {
-    list = false,
-    relativenumber = false,
-    number = false,
-    winfixwidth = true,
-    winfixheight = true,
-    wrap = false,
-    signcolumn = "auto"
-  })
-  end
-  window.gdb_bufnr = vim.api.nvim_create_buf(false, true)
-  window.stdout_bufnr = window.stdout_bufnr == nil and vim.api.nvim_create_buf(false, true) or window.stdout_bufnr
-  window.mi_bufnr = window.mi_bufnr == nil and vim.api.nvim_create_buf(false, true) or window.mi_bufnr
-  vim.api.nvim_set_current_buf(window.gdb_bufnr)
-  local stdout_chan_id = vim.fn.jobstart({"cat", "-"}, {
-    pty = true,
-    on_stdout = function (_, msg, _)
-      io.write(window.stdout_bufnr .. '\n')
-      io.write(vim.api.nvim_buf_line_count(window.stdout_bufnr) .. '\n')
-      for _, line in pairs(msg) do
-        vim.fn.appendbufline(window.stdout_bufnr, vim.api.nvim_buf_line_count(window.stdout_bufnr), line)
-      end
-    end,
-    on_stderr = function (_, _, _)
-    end,
-    on_exit = function (_, _, _)
-      window.stdout_pty = nil
-      vim.cmd('bwipeout! ' .. window.stdout_bufnr)
-    end
-  })
-  window.stdout_pty = vim.api.nvim_get_chan_info(stdout_chan_id).pty
-  local mi_chan_id = vim.fn.jobstart({"cat", "-"}, {
-    pty = true,
-    on_stdout = function (_, msg, _)
-      io.write("mi_pty\n")
-      for _, line in pairs(msg) do
-        vim.fn.appendbufline(window.stdout_bufnr, vim.api.nvim_buf_line_count(window.stdout_bufnr), line)
-      end
-    end,
-    on_stderr = function (_, _, _)
-    end,
-    on_exit = function (_, _, _)
-      window.mi_pty = nil
-      vim.cmd('bwipeout! ' .. window.mi_bufnr)
-    end
-  })
-  window.mi_pty = vim.api.nvim_get_chan_info(mi_chan_id).pty
-
-  local cmd = gdb .. " " .. gdb_args .. " -tty " .. window.stdout_pty .. " " .. app_path
-
-  window.term_chan_id = window.term_chan_id == nil and vim.fn.termopen(cmd, {
-    on_exit = function (_, _, _)
-      window.term_chan_id = nil
-      vim.cmd('bwipeout! ' .. window.gdb_bufnr)
-    end
-  }) or window.term_chan_id
-  vim.api.nvim_chan_send(window.term_chan_id, "server new-ui mi " .. window.mi_pty .. " \n")
-  vim.api.nvim_buf_set_option(window.gdb_bufnr, "filetype", "gdb-terminal")
-  return gdb_win_id
-end
-
-function window:open(opts)
+function window.open(opts)
   opts = opts or { pos = "bottom", size = 20, pos_type = "relative", filetype = "" }
   if opts.bufnr == nil then
-    return nil, nil
+    opts.bufnr = vim.api.nvim_create_buf(false, true)
   end
   local filetype = opts.filetype or ""
-  local size = opts.size or 20
+  local size = opts.size == nil and 20 or opts.size
   local pos_type = opts.pos_type or "absolute"
   local win_id
   local pos = opts.pos or "bottom"
   if pos == "bottom" or pos == "top" then
-    win_id = window.horizontal_layout(window, size, pos, pos_type, {
+    win_id = window.horizontal_layout(size, pos, pos_type, {
     list = false,
     relativenumber = false,
     number = false,
     winfixwidth = true,
-    winfixheight = true,
+    winfixheight = false,
     wrap = false,
     signcolumn = "auto"
   })
   else
-    win_id = window.vertical_layout(window, size, pos, pos_type, {
+    win_id = window.vertical_layout(size, pos, pos_type, {
     list = false,
     relativenumber = false,
     number = false,
@@ -156,7 +73,7 @@ function window:open(opts)
   end
   vim.api.nvim_set_current_buf(opts.bufnr)
   vim.api.nvim_buf_set_option(opts.bufnr, "filetype", filetype)
-  return win_id
+  return opts.bufnr, win_id
 end
 
 return window
